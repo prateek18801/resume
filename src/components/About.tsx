0b4ee5c0a33589b968d299aef01d234e128476e8
Prateek Chaurasia <prateek18801@gmail.com>
2023-09-23 15:47:58 +0530

    Add About section

---
+++ src/components/About.tsx
@@ -0,0 +1,7 @@
+const About = () => {
+    return (
+        <div className='mb-5'>As an aspiring full-stack developer, I build and maintain both the front-end and back-end of websites and applications using Nodejs, Reactjs, MongoDB and other technologies. I focus on developing user interfaces that are easy to use and visually appealing, as well as building back-end systems that are efficient and scalable.</div>
+    );
+}
+
+export default About;
