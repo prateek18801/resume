1ba63dd1b6e9043aaf23a2ed666ea9a949e84183
Prateek Chaurasia <prateek18801@gmail.com>
2023-09-23 15:52:16 +0530

    Update app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+// import About from "./components/About";
+import Achievements from "./components/Achievements";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+
 const App = () => {
     return (
-        <h1 className='text-center'>
-            Hello world!
-        </h1>
+        <div className='flex justify-center bg-gray-900 print:bg-white'>
+            <div className='w-3/5 p-4 m-4 leading-5 shadow-lg rounded-sm bg-white print:m-0 print:p-0 print:w-auto print:shadow-none'>
+                <Header />
+                <Skills />
+                {/* <About /> */}
+                <Projects />
+                <Experience />
+                <Education />
+                <Achievements />
+            </div>
+        </div>
     );
 }
 
