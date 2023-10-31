f5e15860714bfbf00d984cb387d657a3d99b59e5
Prateek Chaurasia <prateek18801@gmail.com>
2023-10-31 18:13:27 +0530

    Add print button

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { AiFillPrinter } from "react-icons/ai";
 // import About from "./components/About";
 import Achievements from "./components/Achievements";
 import Education from "./components/Education";
@@ -8,11 +9,12 @@ import Skills from "./components/Skills";
 
 const App = () => {
     return (
-        <div className='flex justify-center bg-gray-900 print:bg-white'>
-            <div className='w-11/12 max-w-screen-lg p-8 m-4 leading-5 bg-white rounded-sm shadow-lg md:scale-100 scale print:m-0 print:p-0 print:w-auto print:shadow-none'>
+        <div className="flex justify-center bg-gray-900 print:bg-white">
+            <div className="relative w-11/12 max-w-screen-lg p-8 m-4 leading-5 bg-white rounded-sm shadow-lg md:scale-100 scale print:m-0 print:p-0 print:w-auto print:shadow-none">
+                <button className='absolute p-2 text-xl text-gray-400 transition-all border rounded-full outline-none border-slate-400 top-6 right-6 hover:text-gray-800 hover:border-gray-800 print:hidden' onClick={() => { window.print() }}><AiFillPrinter /></button>
                 <Header />
-                <Skills />
                 {/* <About /> */}
+                <Skills />
                 <Projects />
                 <Experience />
                 <Education />
