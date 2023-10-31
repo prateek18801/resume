70374f477f208a1ca2ee4f6873e63b7bd0a929b6
Prateek Chaurasia <prateek18801@gmail.com>
2023-10-31 13:13:43 +0530

    Update skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,11 +1,23 @@
 const Skills = () => {
-    const skills = ['C/C++', 'Javascript', 'Typescript', 'Reactjs', 'Nextjs', 'Nodejs', 'Expressjs', 'MongoDB', 'SQL', 'UI/UX Design', 'Figma'];
+    const skills = [
+        'C/C++',
+        'Javascript',
+        'Typescript',
+        'Reactjs',
+        'Nodejs',
+        'Expressjs',
+        'MongoDB',
+        'SQL',
+        'REST APIs',
+        'Web Scokets',
+        'Figma'
+    ];
 
     return (
         <section className='mb-5'>
             <h2 className='font-mono font-bold text-orange-500'>SKILLS</h2>
             <ul className='flex flex-wrap'>
-                {skills.map(skill => <li className='mr-1 mt-1 px-3 py-1 rounded-md bg-gray-200'>{skill}</li>)}
+                {skills.map(skill => <li className='px-3 py-1 mt-1 mr-1 bg-gray-200 rounded-md'>{skill}</li>)}
             </ul>
         </section>
     );
