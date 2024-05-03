980a42cdcf52d68420420eec6bef28ba29bfd82c
Prateek Chaurasia <prateek18801@gmail.com>
2024-05-03 23:42:44 +0530

    Remove description

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -5,24 +5,17 @@ const Education = () => {
     const education = [
         {
             institution: "Dr. A.P.J. Abdul Kalam Technical University",
-            degree: "Bachelor of Technology - B.Tech, Computer Science",
+            degree: "Bachelor of Technology - B.Tech, Computer Science (SGPA 8.6)",
             location: "Ghaziabad, Uttar Pradesh",
-            duration: "July 2020 - July 2024 (Expected)",
-            description: [
-                "Current SGPA (Avg.) - 8.6",
-                "Courses: Data Structures, Algorithms, DBMS, OS, Software Engineering, Project Management",
-                "Awarded for 100% attendance in 3rd and 5th semester."
-            ]
+            duration: "July 2020 - July 2024",
+            description: []
         },
         {
             institution: "Aditya Birla Public School",
-            degree: "Senior Secondary - Intermediate, Computer Science",
+            degree: "Senior Secondary - Intermediate (95.2%)",
             location: "Jagdishpur, Uttar Pradesh",
             duration: "March 2019 - March 2020",
-            description: [
-                "Percentage - 95.2%",
-                "Active member of Dramatics and Ecology club"
-            ]
+            description: []
         }
     ];
 
