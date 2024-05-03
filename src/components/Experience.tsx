5648b00de3f1db1a22c4cf4592d87fc97b1635b8
Prateek Chaurasia <prateek18801@gmail.com>
2024-05-03 23:42:05 +0530

    Add new experiences

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -4,28 +4,46 @@ const Experience = () => {
 
     const experiences = [
         {
-            position: "Full-Stack Web Developer",
-            company: "Big Data Centre of Excellence",
-            location: "Remote, Uttar Pradesh",
-            duration: "September 2021 - Present (Ongoing)",
-            description: [
-                "Built the official website backend and an administrative dashboard, a web-based attendance portal to track members' daily attendance, increasing productivity by 15%.",
-                "Planned and organized more than six events, including workshops, hackathons, and coding contests for college students, with an average of 250 participants per event.",
-                "Led a team of 6 developers to build a registrar portal and TnP cell portal for a college, streamlining the current processes."
-            ]
+          position: "Freelance Software Developer",
+          company: "SND Digital Pvt. Ltd.",
+          location: "Hybrid, Ghaziabad",
+          duration: "January 2024 - Present (Ongoing)",
+          description: [
+            "Developed 3 softwares for shipment tracking, inventory management and production management.",
+            "Leveraged insights gained from on-site visits to enhance understanding of e-commerce, shipping, and returns management, resulting in better integration of manual processes with software solutions for improved efficiency.",
+          ],
         },
         {
-            position: "Summer Trainee",
-            company: "Indorama India Pvt. Ltd.",
-            location: "On-Site, Uttar Pradesh",
-            duration: "July 2023 - August 2023",
-            description: [
-                "Worked on the library management system using SQL, reducing the time it takes to process library transactions by 20%.",
-                "Successfully implemented a new Agile workflow for the library management system.",
-                "Analyzed how information technology integrates with manufacturing industries and identified potential improvements that could be made to current working processes, which could save the company revenue in the long run."
-            ]
-        }
-    ];
+          position: "Application Engineering Intern",
+          company: "Fusionflow Technologies Pvt. Ltd.",
+          location: "Remote, Hyderabad",
+          duration: "December 2023 - Feburary 2024",
+          description: [
+            "Created POCs and demos for an API integration platform, demonstrating a strong understanding of OAuth2 and REST APIs.",
+            "Gained experience working with Kafka, microservices architectures, Kong API Gateway, and microservice orchestration platforms.",
+          ],
+        },
+        {
+          position: "Full Stack Web Developer",
+          company: "Glimpse Education Pvt. Ltd.",
+          location: "Remote, Gurugram",
+          duration: "October 2023 - December 2023",
+          description: [
+            "Designed and developed the company's landing page.",
+            "Built back-end functionalities for data processing and address validation in large CSV files.",
+          ],
+        },
+        {
+          position: "Summer Intern",
+          company: "Indorama India Pvt. Ltd.",
+          location: "On-Site, Jagdishpur (UP)",
+          duration: "July 2023 - August 2023",
+          description: [
+            "Developed and implemented SQL procedures and triggers.",
+            "Observed how IT departments integrate with manufacturing operations and workflows. Gained valuable insights into corporate structure and interdepartmental collaboration. Participated in a field visit to the fertilizer manufacturing plant.",
+          ],
+        },
+      ];
 
     return (
         <section className='mb-5'>
